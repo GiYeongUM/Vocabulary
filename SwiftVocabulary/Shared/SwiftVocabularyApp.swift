96a5ccea9deaf1c626ba880fdba4a8e8b4dbd0ea
GiYeong Um <eomky2005@gmail.com>
2022-04-06 23:50:06 +0900

    [update] 프로젝트 시작
    SwiftUI 학습 프로젝트 시작

---
+++ SwiftVocabulary/Shared/SwiftVocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftVocabularyApp.swift
+//  Shared
+//
+//  Created by 엄기영 on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftVocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
